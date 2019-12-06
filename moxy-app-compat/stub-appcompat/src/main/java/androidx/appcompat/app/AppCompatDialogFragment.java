20a380204b8eb26ee07474b5f4eb6a6a534794a0
eugene <eugene.egoshin@moneta.ru>
2019-12-06 11:27:21 +0300

    AndroidX

---
+++ moxy-app-compat/stub-appcompat/src/main/java/androidx/appcompat/app/AppCompatDialogFragment.java
@@ -0,0 +1,6 @@
+package androidx.appcompat.app;
+
+import androidx.fragment.app.DialogFragment;
+
+public class AppCompatDialogFragment extends DialogFragment {
+}
---
+++ moxy-app-compat/stub-appcompat/src/main/java/androidx/fragment/app/FragmentActivity.java
@@ -0,0 +1,11 @@
+package androidx.fragment.app;
+
+/**
+ * Created by senneco on 11.10.2016
+ */
+
+public class FragmentActivity {
+	public boolean isFinishing() {
+		throw new RuntimeException("Stub!");
+	}
+}
